96c342e2a79df5812a99756bbeb27822f4985e97
AbleLincoln <maneyandrew@gmail.com>
2022-10-11 23:15:03 -0700

    init

---
+++ src/App.vue
@@ -0,0 +1,133 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+import HelloWorld from "./components/HelloWorld.vue";
+import ShowsView from "./views/ShowsView.vue";
+</script>
+
+<script>
+export default {
+  data() {
+    return {
+      overlayOpacity: 0,
+    };
+  },
+
+  methods: {
+    handleScroll(event) {
+      const SCALE_FACTOR = 0.6;
+      const MAX_OPACITY = 0.5;
+      const scrollPercent = event.target.scrollTop / event.target.offsetHeight;
+      this.overlayOpacity = Math.min(scrollPercent * SCALE_FACTOR, MAX_OPACITY);
+    },
+  },
+};
+</script>
+
+data() { }
+
+<template>
+  <div
+    id="container"
+    @scroll="handleScroll"
+    :style="{
+      backgroundImage: `linear-gradient(rgba(0, 0, 0, ${overlayOpacity}), rgba(0, 0, 0, ${overlayOpacity})), url(/cover.jpg)`,
+    }"
+  >
+    <header>
+      <!-- <div class="wrapper"> -->
+      <h1>Wyman & the Wolves</h1>
+
+      <!-- <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav> -->
+      <!-- </div> -->
+    </header>
+
+    <ShowsView />
+
+    <!-- <RouterView /> -->
+  </div>
+</template>
+
+<style scoped>
+#container {
+  height: 100vh;
+  overflow-y: scroll;
+
+  background-size: cover;
+}
+
+header {
+  line-height: 1.5;
+  height: 100vh;
+  width: 100vw;
+
+  color: var(--color-heading);
+
+  display: flex;
+  justify-content: center;
+  padding-top: 20vh;
+}
+
+h1 {
+  font-size: 7.5vw;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  /* header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  } */
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+}
+</style>
---
+++ src/views/ShowsView.vue
@@ -0,0 +1,133 @@
+<script setup>
+import { format } from "date-fns";
+</script>
+
+<script>
+export default {
+  data() {
+    return {
+      shows: [
+        {
+          date: "2022/10/16",
+          time: "1pm",
+          venue: "Mother's Tavern",
+          location: "Sunset Beach",
+        },
+        {
+          date: "2022/10/17",
+          time: "9pm",
+          venue: "Harvard and Stone",
+          location: "East Hollywood",
+        },
+        {
+          date: "2022/10/21",
+          time: "nighttime",
+          venue: "Silverlake Lounge",
+          location: "Silverlake",
+        },
+        {
+          date: "2022/10/22",
+          time: "3pm",
+          venue: "Salty Bear Brewing",
+          location: "Costa Mesa",
+        },
+        {
+          date: "2022/10/23",
+          time: "7pm",
+          venue: "Skip the Record",
+          location: "secret location",
+        },
+        {
+          date: "2022/10/24",
+          time: "9pm",
+          venue: "Harvard and Stone",
+          location: "East Hollywood",
+        },
+        {
+          date: "👻 Halloween 🎃",
+          time: "9pm",
+          venue: "Harvard and Stone",
+          location: "East Hollywood",
+        },
+        {
+          date: "2022/11/02",
+          time: "9pm",
+          venue: "Good Times at Davey Wayne's",
+          location: "Hollywood",
+        },
+        {
+          date: "2022/11/06",
+          time: "8pm",
+          venue: "Redwood Bar",
+          location: "DTLA",
+        },
+      ],
+    };
+  },
+
+  methods: {
+    formatDate(date) {
+      const _date = new Date(date);
+      if (_date == "Invalid Date") return date;
+      const dayOfWeek = format(_date, "cccc");
+      const month = format(_date, "MMMM");
+      const dayOfMonth = this.formatOrdinal(format(_date, "do"));
+
+      return `${dayOfWeek}, <span class="nowrap">${month} ${dayOfMonth}</span>`;
+    },
+    formatOrdinal(date) {
+      return date.replace(/(\D+)/, "<sup>$1</sup>");
+    },
+  },
+};
+</script>
+
+<template>
+  <section>
+    <h2>Shows</h2>
+
+    <div v-for="(show, i) in shows" :key="i" class="show">
+      <p>
+        <span v-html="formatDate(show.date)"></span><br /><small>{{
+          show.time
+        }}</small>
+      </p>
+      <p>
+        {{ show.venue }} <br /><small>{{ show.location }}</small>
+      </p>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+h2 {
+  text-align: center;
+  margin-bottom: 1em;
+}
+
+section {
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 48px 2vw 140px;
+  font-size: 24px;
+}
+
+.show {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-gap: 25px;
+  margin-bottom: 40px;
+}
+
+.show p {
+  line-height: 1.25;
+}
+
+.show p:first-child {
+  text-align: right;
+}
+
+.nowrap {
+  white-space: nowrap;
+}
+</style>
