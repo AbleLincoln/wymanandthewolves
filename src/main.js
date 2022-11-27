34d2fd030f9f456388d8af235cab6c6fd42b3357
AbleLincoln <maneyandrew@gmail.com>
2022-11-27 17:15:00 -0500

    remove starter code

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import router from "./router";
 
 import "./assets/main.css";
 
@@ -20,7 +19,5 @@ library.add(faInstagram, faSpotify, faApple, faYoutube);
 const app = createApp(App);
 
 app.component("fa-icon", FontAwesomeIcon);
-
-app.use(router);
 
 app.mount("#app");
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
+/**
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -21,3 +21,4 @@ const router = createRouter({
 });
 
 export default router;
+*/
