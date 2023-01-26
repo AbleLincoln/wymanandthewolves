2347857a31e43d7eaf831d7e0f8d366d15543138
AbleLincoln <maneyandrew@gmail.com>
2023-01-25 16:17:08 -0800

    i think debug should be fine now

--- src/main.js
+++ src/main.js
@@ -24,8 +24,6 @@ library.add(faInstagram, faSpotify, faApple, faYoutube);
 /* create app */
 const app = createApp(App);
 
-console.log(import.meta.env.DEV);
-
 app.use(VueFire, {
   firebaseApp,
   modules: [
@@ -34,7 +32,7 @@ app.use(VueFire, {
         "6Le0SOUiAAAAAODZ4xtfy6wJyo4gr0b_yMy9F-ur"
       ),
       isTokenAutoRefreshEnabled: true,
-      // debug: !!import.meta.env.DEV,
+      debug: import.meta.env.DEV,
     }),
     VueFireAuth(),
   ],
